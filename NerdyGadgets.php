03783298642a2afd262c6fe8666ac8d513903b96
Maikel <30528871+maikelmo@users.noreply.github.com>
2019-05-20 12:18:43 +0200

    Add files via upload

---
+++ NerdyGadgets.php
@@ -0,0 +1,59 @@
+<?php
+require_once 'includes/db.php';
+
+$ophalen = $dbh->prepare("SELECT * FROM mensen");
+$ophalen->execute();
+$content = $ophalen->fetchAll();
+?>
+
+<head>
+    <title>ICTM2I Groep Website</title>
+    <link rel="stylesheet" href="css/table.css" type="text/css"> 
+</head>
+<body>
+
+<table>
+    <tr>
+        <th>Naam</th>
+        <th>Functie</th>
+        <th>Wijzigen</th>
+        <th>Verwijderen</th>
+    </tr>
+    <?php foreach($content as $row): ?>
+        <tr>
+            <th> <?= $row['naam'] ?> </th>
+            <th> <?= $row['functie'] ?> </th>
+            <th>
+                <form method="post" action="includes/wijzigen.php"></th>
+                    <label for="wijzigen">Wijzigen</label>
+                    <select name="wijzigen" id="wijzigen" onchange="this.form.submit()">
+                        <option>Teamleider</option>
+                        <option>Notulist</option>
+                        <option>Kwaliteitbeheer</option>
+                        <option>Planner</option>
+                    </select>'.
+                    <input type="hidden" name="ID" value="<?= $row['id'] ?>">
+                </form>
+            </th>
+            <th>
+                <form method="post" action="includes/verwijder.php">
+                    <input type="hidden" name="ID_1" value="<?= $row['id'] ?>">
+                    <input type="submit" name="verwijder" value="Verwijderen">
+                </form>
+            </th>
+        </tr>
+    <?php endforeach; ?>
+</table>
+
+<form method="post" action="includes/toevoegen.php">
+    <label for="name">Naam:</label><input type="text" name="toevoegen" id="name" placeholder="Vul hier de naam in">
+    <label for="functie">Functie:</label>
+    <select id="functie" name="functie_toevoegen">
+        <option>Teamleider</option>
+        <option>Notulist</option>
+        <option>Kwaliteitbeheer</option>
+        <option>Planner</option>
+    </select>
+    <input type="submit" value="Verzenden" name="Verzend">
+</form>
+</body>
